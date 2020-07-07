befa3cdf17f5e2743a167f7d90db1e93b556e464
Aaron <a.akuzum@gmail.com>
2020-07-07 10:04:25 -0700

    fix

--- app.js
+++ app.js
@@ -23,4 +23,4 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
